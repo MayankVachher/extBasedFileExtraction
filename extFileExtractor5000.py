2344b61b844ee5849d51fc5f981eed852c35161d
Mayank Vachher <mayankv0207@gmail.com>
2015-06-20 23:48:52 +0530

    Create extFileExtractor5000.py

---
+++ extFileExtractor5000.py
@@ -0,0 +1,21 @@
+import os
+import shutil 
+
+# Set the directory you want to start from
+rootDir = './spark-master'
+# Set the directory you want to gather files to
+copyDir = './datacollection'
+# Set the file extension you want to search for
+ext = ".java"
+
+count = 1
+for dirName, subdirList, fileList in os.walk(rootDir):
+    for fname in fileList:
+        if fname.endswith(ext):
+        	srcFile = os.path.join(dirName, fname)
+        	if os.path.exists(os.path.join(copyDir, fname)):
+        		fname = fname.rstrip(ext)+"(1)"+ext
+        	dstFile = os.path.join(copyDir, fname)	
+        	print str(count)+". Copying File "+srcFile+" as "+dstFile+" ..."
+        	shutil.copy2(srcFile, dstFile)
+        	count += 1
