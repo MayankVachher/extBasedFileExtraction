fa0445cb62fa20afcca48e9f24ca2326465600e9
Mayank Vachher <mayankv0207@gmail.com>
2015-06-20 23:56:56 +0530

    Update extFileExtractor5000.py
    Add functional capabilities

--- extFileExtractor5000.py
+++ extFileExtractor5000.py
@@ -1,21 +1,25 @@
 import os
 import shutil 
 
-# Set the directory you want to start from
-rootDir = './spark-master'
-# Set the directory you want to gather files to
-copyDir = './datacollection'
-# Set the file extension you want to search for
-ext = ".java"
+def extractor5000():
+    # Set the directory you want to start from
+    rootDir = './spark-master'
+    # Set the directory you want to gather files to
+    copyDir = './datacollection'
+    # Set the file extension you want to search for
+    ext = ".java"
+    
+    count = 1
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        for fname in fileList:
+            if fname.endswith(ext):
+            	srcFile = os.path.join(dirName, fname)
+            	if os.path.exists(os.path.join(copyDir, fname)):
+            		fname = fname.rstrip(ext)+"(1)"+ext
+            	dstFile = os.path.join(copyDir, fname)	
+            	print str(count)+". Copying File "+srcFile+" as "+dstFile+" ..."
+            	shutil.copy2(srcFile, dstFile)
+            	count += 1
 
-count = 1
-for dirName, subdirList, fileList in os.walk(rootDir):
-    for fname in fileList:
-        if fname.endswith(ext):
-        	srcFile = os.path.join(dirName, fname)
-        	if os.path.exists(os.path.join(copyDir, fname)):
-        		fname = fname.rstrip(ext)+"(1)"+ext
-        	dstFile = os.path.join(copyDir, fname)	
-        	print str(count)+". Copying File "+srcFile+" as "+dstFile+" ..."
-        	shutil.copy2(srcFile, dstFile)
-        	count += 1
+if __name__ == '__main__':
+    extractor5000()
